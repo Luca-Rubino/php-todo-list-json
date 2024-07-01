75e77a6a00d3767ce6daf7fa5649255280d3cd8b
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-01 20:39:08 +0200

    configurazione file base index con i collegamenti ad i vari script e font

--- src/index.php
+++ src/index.php
@@ -1,5 +1,43 @@
 <?php 
 
-header("location: page/server.php")
+// header("location: page/server.php")
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- personal css -->
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Todolist-Api</title>
+</head>
+<body>
+    <header>
+        <h1>Todolist-Api</h1>
+    </header>
+    <main id="app">
+        <section>
+            <h2>Lista impegni</h2>
+        </section>
+        <section>
+            <ul>
+                <li></li>
+            </ul>
+        </section>
+    </main>
+    <footer></footer>
+    <!-- axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- vue3 -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- boodsrap -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- personal js -->
+     <script src="./js/script.js"></script>
+</body>
+</html>
